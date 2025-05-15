f68262e45c6f2f4bafe42a3b0671d78b8bd12693
RoutinesInTheNight <yeahup@icloud.com>
2025-05-16 02:12:16 +0300

    Add files via upload

---
+++ best-and-worst/movies/scripts.js
@@ -0,0 +1,116 @@
+const telegram = window.Telegram.WebApp;
+const DEVICE_TYPE = telegram.platform;
+
+telegram.expand();
+if (telegram.isVersionAtLeast("6.1")) telegram.BackButton.hide();
+if (telegram.isVersionAtLeast("7.7")) telegram.disableVerticalSwipes();
+if (telegram.isVersionAtLeast("8.0")) {
+  telegram.requestFullscreen();
+  telegram.lockOrientation();
+}
+
+
+
+
+const children = document.querySelectorAll('body > *');
+children.forEach((child, index) => {
+  setTimeout(() => {
+    child.classList.add('visible');
+  }, index * 25);
+});
+
+
+
+
+
+
+
+
+
+
+
+function hapticFeedback(type, redirectUrl) {
+  if (telegram.isVersionAtLeast("6.1") && (DEVICE_TYPE === 'android' || DEVICE_TYPE === 'ios')) {
+    switch (type) {
+      case 'light':
+        telegram.HapticFeedback.impactOccurred('light');
+        break;
+      case 'medium':
+        telegram.HapticFeedback.impactOccurred('medium');
+        break;
+      case 'heavy':
+        telegram.HapticFeedback.impactOccurred('heavy');
+        break;
+      case 'rigid':
+        telegram.HapticFeedback.impactOccurred('rigid');
+        break;
+      case 'soft':
+        telegram.HapticFeedback.impactOccurred('soft');
+        break;
+      case 'error':
+        telegram.HapticFeedback.notificationOccurred('error');
+        break;
+      case 'success':
+        telegram.HapticFeedback.notificationOccurred('success');
+        break;
+      case 'warning':
+        telegram.HapticFeedback.notificationOccurred('warning');
+        break;
+      case 'change':
+        telegram.HapticFeedback.selectionChanged();
+        break;
+      default:
+        console.warn('Unknown haptic feedback type:', type);
+    }
+  }
+
+  console.log(redirectUrl )
+  if (redirectUrl && redirectUrl !== '#') {
+    const children = document.querySelectorAll('body > *');
+    children.forEach((child, index) => {
+      setTimeout(() => {
+        child.classList.remove('visible');
+      }, index * 5);
+    });
+
+    setTimeout(() => {
+      window.location.href = redirectUrl;
+    }, children.length * 5);
+  }
+}
+
+
+
+
+
+
+// Нижний паддинг в нижнем меню с учётом безопасной зоны
+document.addEventListener('DOMContentLoaded', () => {
+  const bottomMenu = document.querySelector('.bottom-menu');
+  let safeAreaBottom = 0;
+  let contentSafeAreaBottom = 0;
+  bottomMenu.style.paddingBottom = '0px';
+  function updatePadding() {
+    const totalPadding = safeAreaBottom + contentSafeAreaBottom;
+    if (totalPadding === 0) {
+      bottomMenu.style.paddingBottom = `0.25rem`;
+    } else {
+      bottomMenu.style.paddingBottom = `${totalPadding}px`;
+    }
+  }
+  function onContentSafeAreaChanged() {
+    const contentSafeArea = telegram.contentSafeAreaInset || {};
+    contentSafeAreaBottom = contentSafeArea.bottom || 0;
+    updatePadding();
+  }
+  function onSafeAreaChanged() {
+    const safeArea = telegram.safeAreaInset || {};
+    safeAreaBottom = safeArea.bottom || 0;
+    updatePadding();
+  }
+  telegram.onEvent('contentSafeAreaChanged', onContentSafeAreaChanged);
+  telegram.onEvent('safeAreaChanged', onSafeAreaChanged);
+  onContentSafeAreaChanged();
+  onSafeAreaChanged();
+});
+
